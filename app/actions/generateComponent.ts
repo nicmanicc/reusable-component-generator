d0e4889df7314f1971ae4d1bb1d89443b38eab39
DESKTOP-GHSG0BR\Nic <nickmanic@hotmail.com>
2025-12-20 01:13:08 +1100

    feat: update openai model behaviour

--- app/actions/generateComponent.ts
+++ app/actions/generateComponent.ts
@@ -3,14 +3,14 @@
 import OpenAI from "openai";
 const client = new OpenAI();
 
-export const generateComponent = async (promptInput: string) => {
+export const generateComponent = async (promptInput: string, code?: string) => {
   const response = await client.responses.create({
     model: "gpt-5-mini",
     prompt: {
       id: "pmpt_694552ca937c8196aad1c80dd2973db2045fb08fa22f3415",
-      version: "2",
+      version: "3",
     },
-    input: promptInput,
+    input: promptInput + " Here is the existing code to refine: " + code,
   });
   return response.output_text;
 };
