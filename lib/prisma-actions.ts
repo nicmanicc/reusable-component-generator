4225ed5ec15e3cfb823a077342706400b13b5f49
DESKTOP-GHSG0BR\Nic <nickmanic@hotmail.com>
2025-12-31 21:51:16 +1100

    feat: implement createProject function for project creation in Prisma

---
+++ lib/prisma-actions.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/utils/prisma/client";
+/* id                 String               @id @default(dbgenerated("gen_random_uuid()")) @db.Uuid
+  user_id            String               @db.Uuid
+  title              String
+  description        String?
+  created_at         DateTime?            @default(now()) @db.Timestamptz(6)
+  chat_messages      chat_messages[]
+  component_versions component_versions[]
+  users   */
+export async function createProject(
+  user_id: string,
+  name: string,
+  description: string
+) {
+  return prisma.projects.create({
+    data: {
+      user_id: user_id,
+      title: name,
+      description: description,
+    },
+  });
+}
